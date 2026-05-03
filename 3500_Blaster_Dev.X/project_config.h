77ba52b8a1da4b267e4b63560050df3ea3884c24
Devan Malik <dgmalik771@icloud.com>
2026-05-03 13:58:10 -0400

    stuff

--- 3500_Blaster_Dev.X/project_config.h
+++
@@ -1,6 +0,0 @@
-#ifndef PROJECT_CONFIG_H
-#define PROJECT_CONFIG_H
-
-#define F_CPU 16000000UL
-
-#endif
--- 3500_Chest_Dev.X/ir_rx.h
+++
@@ -1,9 +0,0 @@
-#ifndef IR_RX_H
-#define IR_RX_H
-
-#include <stdint.h>
-
-void ir_rx_init(void);
-uint8_t ir_rx_signal_present(void);
-
-#endif
